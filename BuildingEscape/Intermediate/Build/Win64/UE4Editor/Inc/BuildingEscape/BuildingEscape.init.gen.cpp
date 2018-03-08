3da2d51785eb49e17230f3a1868957daa4302841
WOOKIEE-DESKTOP\mriml <mrimler1983@gmail.com>
2018-03-08 12:55:21 -0600

    some more statues

--- BuildingEscape/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/BuildingEscape.init.gen.cpp
+++ BuildingEscape/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/BuildingEscape.init.gen.cpp
@@ -19,7 +19,7 @@ void EmptyLinkFunctionForGeneratedCodeBuildingEscape_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/BuildingEscape",
 				PKG_CompiledIn | 0x00000000,
-				0x1D50C9AB,
+				0xF6814DAC,
 				0xEEE69D44,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
